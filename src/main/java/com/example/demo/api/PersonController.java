4a2ede6f8857354c5fafecdded272923f0ff0c80
guitiorr <officialglude12345@gmail.com>
2024-11-14 13:16:04 +0700

    get all person done

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -4,10 +4,9 @@ import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RequestMapping("api/v1/person")
@@ -24,5 +23,10 @@ public class PersonController {
     @PostMapping
     public void addPerson(@RequestBody Person person){
         personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
     }
 }
